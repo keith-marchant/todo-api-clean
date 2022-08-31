264f132c42b6ec65fa3eba2c501e54ad5d7d7a50
Keith Marchant <kemarchant@outlook.com>
2022-08-31 20:31:57 +1000

    Added an API Version endpoint

---
+++ src/TodoApp.Api/Controllers/VersionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using TodoApp.Application.Entities;
+
+namespace TodoApp.Api.Controllers
+{
+    [AllowAnonymous]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("[Controller]")]
+    public class VersionController : ControllerBase
+    {
+        [ProducesResponseType(typeof(ApiInformation), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet]
+        public ActionResult<string> GetApiVersion()
+        {
+            return Ok(new ApiInformation(Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly()));
+        }
+    }
+}
---
+++ src/TodoApp.Application/Entities/ApiInformation.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace TodoApp.Application.Entities
+{
+    public class ApiInformation
+    {
+        public ApiInformation(Assembly assembly)
+        {
+            if (assembly == null)
+            {
+                throw new ArgumentNullException(nameof(assembly));
+            }
+
+            Name = assembly.GetName().Name;
+            Version = assembly.GetName().Version?.ToString();
+        }
+
+        public string? Name { get; set; }
+        public string? Version { get; set; }
+    }
+}
